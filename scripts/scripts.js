50586510633b0e172b41c9a94b64c6329ceabc6f
SlavikGemay <gemayit@gmail.com>
2024-08-21 21:42:20 +0300

    dz 15 exp update hovers

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:4
+        }
+    }
+
+})
